7ea0b13c7038e2635df5861199b91cb8dcc93dce
Yasir Kazmi <37070387+yasir-yk@users.noreply.github.com>
2024-10-07 11:31:33 -0700

    updatecode_10-07-2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         primary: '#0C336B',  // Customize with your desired primary color
         secondary: '#D8BB7A', // Customize with your desired secondary color
         light: '#F6FAF1',
+        
       },
       fontFamily: {
         poppins: ['"Poppins"', 'sans-serif'],
@@ -14,6 +15,10 @@ module.exports = {
       },
       backgroundImage: {
         'hover-gradient': 'linear-gradient(180deg, #FBE39B 0%, #A5824B 100%)',
+        'custom-gradient': 'linear-gradient(86.95deg, #D8BB7A 2.52%, #0C336B 97.47%)',
+      },
+      boxShadow: {
+        'customShadow': '0 4px 40px 0 rgba(0, 0, 0, 0.1)',
       },
     },
   },
